d6539a2f0749b41c01c8d9b1a319c236172d0dfe
Zaatar <hani.zaatar.92@gmail.com>
2021-11-03 00:29:28 +0100

    Initial commit, openGL window init

---
+++ scripts/build.bat
@@ -0,0 +1,23 @@
+@echo off
+
+:: Create build dir
+set buildDir=%~dp0..\build
+if not exist %buildDir% mkdir %buildDir%
+pushd %buildDir%
+
+:: Create obj dir
+set objDir=.\obj
+if not exist %objDir% (
+    mkdir %objDir%
+    mkdir %objDir%\engine
+    mkdir %objDir%\game
+)
+
+::Needed folder
+set scriptDir=%~dp0..\scripts
+
+::Use make to build default target
+cd %scriptDir%\
+mingw32-make
+
+popd
